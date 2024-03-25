9a52eb4fd573b0d12c60e45a9115394f953754bf
e-V01 <91158407+e-V01@users.noreply.github.com>
2024-03-25 23:00:20 +0200

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Y K on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
